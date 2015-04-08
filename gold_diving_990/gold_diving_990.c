64b1670217bdbe9f2621d7d4a69a3a538cf7e166
Darrien Glasser <Darrienglasser@outlook.com>
2015-04-08 17:01:52 -0400

    Gold diving almost working.

--- gold_diving_990/gold_diving_990.c
+++ gold_diving_990/gold_diving_990.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
 #ifndef max
-#define max (a, b) (((a) > (b)) ? (a):(b))
+#define max(a, b) (((a)>(b)) ? (a):(b))
 #endif
+*/
 
 int answers[31][1001] = {0};
-int d[30];
-int v[30];
+int d[31];
+int v[31];
 
-int getMostGold(int timeLeft, int numThings);
+int getMostGold(int timeLeft, int numThings, int w);
+int myMax(int a, int b){
+
+	return (a > b) ? a:b;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -27,21 +33,30 @@ int main(int argc, char const *argv[])
 
 	for (i = 0; i < numTreasures; ++i)
 	{
-		scanf("%d%d", d[i], v[i]);
+		scanf("%d%d", &d[i + 1], &v[i + 1]);
 	}
+	printf("%d\n", getMostGold(airTime, numTreasures, wConstant));
 	return 0;
 }
 
-int getMostGold(int timeLeft, int numThings){
+int getMostGold(int timeLeft, int numThings, int w){
+	int factor = w * 3;
+
+	printf("\nTime left: %d  numThing: %d  w: %d\n", timeLeft, numThings, w);
+
 	if (numThings < 0 || timeLeft < 0)
 	{
+		printf("Checking 1\n");
 		return 0;
 	}
 	else if (answers[numThings][timeLeft] != -1)
 	{
+		printf("Checking 2\n");
+		printf("Returning: %d\n", answers[numThings][timeLeft]);
 		return answers[numThings][timeLeft];
 	}
 	else{
-		return answers[numThings][timeLeft] = max(v[numThings] + getMostGold(timeLeft - d[numThings], numThings - 1)), getMostGold(timeLeft, numThings - 1);
+		printf("Checking 3\n");
+		return answers[numThings][timeLeft] = myMax(v[numThings] + getMostGold(timeLeft - (d[numThings] * factor), numThings - 1, w), getMostGold(timeLeft, numThings - 1, w));
 	}
 }
